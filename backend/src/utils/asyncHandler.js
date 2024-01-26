e49f18949b29a5372e0fcb507aea4536ab9c80c5
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-01-26 23:32:40 +0530

    Fix INVALID_STATUS_CODE bug

--- backend/src/utils/asyncHandler.js
+++ backend/src/utils/asyncHandler.js
@@ -3,11 +3,11 @@ const asyncHandler = (callbackFn) => {
     try {
       await callbackFn(req, res, next);
     } catch (error) {
-      res.status(error.statusCode).json({
+      res.status(error.statusCode || 500).json({
         success: error.success,
         message: error.message,
       });
-      // throw err; // IMPORTANT:  The backend will stop forever once any error is found in any API-route
+      // throw error; // IMPORTANT:  The backend will stop forever once any error is found in any API-route
     }
   };
 };
