bdb3f92d2741af415e6330d799d4956ac92b5c70
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-01-31 17:05:43 +0530

    Write mongoose custom methods for token generation and password validation

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import customError from "../utils/customError.js";
 
 const userSchema = new mongoose.Schema(
   {
@@ -25,6 +27,10 @@ const userSchema = new mongoose.Schema(
       required: false,
       trim: true,
     },
+    refreshToken: {
+      type: String,
+      required: false,
+    },
   },
   { timestamps: true }
 );
@@ -37,6 +43,45 @@ userSchema.pre("save", async function () {
   const hashedPassword = await bcrypt.hash(this.password, 10);
   this.password = hashedPassword;
 });
+
+// MONGOOSE CUSTOM METHOD: Method for comaparing the pasword given by the user and the encrypted password stored in the database.
+userSchema.methods.validatePassword = async function (password) {
+  if (!password) {
+    throw new customError(401, "Password not provided");
+  }
+  const isPasswordCorrect = await bcrypt.compare(password, this.password);
+  return isPasswordCorrect;
+};
+
+// MONGOOSE CUSTOM METHOD: Method for generating access token
+userSchema.methods.generateAccessToken = async function () {
+  const accessToken = await jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+  return accessToken;
+};
+
+// MONGOOSE CUSTOM METHOD: Method for generating refresh token
+userSchema.methods.generateRefreshToken = async function () {
+  const refreshToken = await jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+  return refreshToken;
+};
 
 const User = mongoose.model("User", userSchema);
 
