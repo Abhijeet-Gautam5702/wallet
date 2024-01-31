7b35bb415dbde5e82a4adfc759bbb7b3b2616014
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-01-31 17:04:15 +0530

    Write zod schema for data validation in case of user-signin

--- backend/src/models/zod.schema.js
+++ backend/src/models/zod.schema.js
@@ -1,11 +1,16 @@
 // Custom schemas for the form-data received from the client (Using Zod)
 import { z } from "zod";
 
-const UserZodSchema = z.object({
+const UserSignUpZodSchema = z.object({
   firstname: z.string(),
   lastname: z.string().optional().or(z.undefined()),
   email: z.string().email(),
   password: z.string(),
 });
 
-export { UserZodSchema };
+const UserSignInZodSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
+export { UserSignUpZodSchema,UserSignInZodSchema };
